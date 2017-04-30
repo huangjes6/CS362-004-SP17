32108bdc50035921d6a80d0d01c2cd64fbeffe12
Jessica Huang <huangjes@flip2.engr.oregonstate.edu>
2017-04-29 20:50:14 -0700

    Added randomquiz folder

---
+++ projects/huangjes/randomquiz/randomstring.c
@@ -0,0 +1,26 @@
+/**********************************************************************************
+** Name: Jessica Huang
+** Course: CS362-004-SP17
+** Description: Explanation for how the solution for testme.c was developed.
+***********************************************************************************/
+
+The inputChar() function is a function that creates random characters. Based on the 
+function testme(), I can see that the characters can be symbols or letters. Therefore,
+using the ASCII chart, I noticed that there are 95 usable ASCII characters, and they
+start at character 32, hence the expression rand() % 95 + 32. This way, the characters
+returned will be able to satisfy one of the conditions in the testme() function.
+
+In the testme() function, the conditions we are looking to satisfy only require an array
+of 5 characters and a null character. Thus, I made my array be 5 characters long with 
+a null character at the end. To satisfy the conditions, all random characters in the
+string are only the lowercase letters of the alphabet. Therefore, for the string random
+I narrowed it down to only pick from the 26 lower case letters of the alphabet. Then,
+I made the last character a null character. 
+
+In testme(), we start at state 0. Once the inputChar() function returns '[', then the 
+state changes to 1. The loop continues and if the conditions are met in the if statement,
+the state increments by 1. This is until state 9 is reached. At this point, the function
+begins checking each index of the string for characters that match the conditional. 
+s[0] == 'r', s[1] == 'e', s[2] == 's', s[3] == 'e', s[4] == 't', and s[5] == '\0'. If this
+condition is randomly met, then the function prints "error " and quits, freeing the malloc'd
+array at the end. 
---
+++ projects/huangjes/randomquiz/testme.c
@@ -0,0 +1,73 @@
+/**********************************************************************************
+** Name: Jessica Huang
+** Course: CS362-004-SP17
+** Description: Implementation of the inputChar() and *inputString() functions
+***********************************************************************************/
+
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<time.h>
+
+char inputChar()
+{
+    // TODO: rewrite this function
+	// The first usable ascii character for this assignment is at decimal 32, so add that.
+	char randChar = rand() % 95 + 32;
+    return randChar;
+}
+
+char *inputString()
+{
+    // TODO: rewrite this function
+	int i;
+	char *str = (char*) malloc(5);
+	for (i = 0; i < 5; i++)
+	{
+		// The first lower case letter starts at 97 and there are 26 letters;
+		str[i] = rand() % 26 + 97;
+	}
+	str[5] = '\0';
+    return str;
+}
+
+void testme()
+{
+  int tcCount = 0;
+  char *s;
+  char c;
+  int state = 0;
+  while (1)
+  {
+    tcCount++;
+    c = inputChar();
+    s = inputString();
+    printf("Iteration %d: c = %c, s = %s, state = %d\n", tcCount, c, s, state);
+    if (c == '[' && state == 0) state = 1;
+    if (c == '(' && state == 1) state = 2;
+    if (c == '{' && state == 2) state = 3;
+    if (c == ' '&& state == 3) state = 4;
+    if (c == 'a' && state == 4) state = 5;
+    if (c == 'x' && state == 5) state = 6;
+    if (c == '}' && state == 6) state = 7;
+    if (c == ')' && state == 7) state = 8;
+    if (c == ']' && state == 8) state = 9;
+    if (s[0] == 'r' && s[1] == 'e'
+       && s[2] == 's' && s[3] == 'e'
+       && s[4] == 't' && s[5] == '\0'
+       && state == 9)
+    {
+      printf("error ");
+      exit(200);
+    }
+  }
+  free(s);
+}
+
+
+int main(int argc, char *argv[])
+{
+    srand(time(NULL));
+    testme();
+    return 0;
+}

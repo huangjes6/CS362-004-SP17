53b51e14e93f8a6ab75fe807c6a2706803c366a7
huangjes6 <huangjes@oregonstate.edu>
2017-04-29 21:28:12 -0700

    Update testme.c

--- projects/huangjes/randomquiz/testme.c
+++ projects/huangjes/randomquiz/testme.c
@@ -11,63 +11,65 @@
 
 char inputChar()
 {
-    // TODO: rewrite this function
-	// The first usable ascii character for this assignment is at decimal 32, so add that.
-	char randChar = rand() % 95 + 32;
-    return randChar;
+	// TODO: rewrite this function
+	// The first usable ascii character for this assignment is a space and there are 95 ascii
+	// characters including letters and symbols.
+	char randChar = ' ' + (rand() % 95);
+	return randChar;
 }
 
 char *inputString()
 {
-    // TODO: rewrite this function
+	// TODO: rewrite this function
 	int i;
-	char *str = (char*) malloc(5);
+	char *str = (char *) malloc(5);
 	for (i = 0; i < 5; i++)
 	{
-		// The first lower case letter starts at 97 and there are 26 letters;
-		str[i] = rand() % 26 + 97;
+		// The first lower case letter is 'a' and there are 26 letters;
+		str[i] = 'a' + (rand() % 26);
 	}
+	// Terminate the string
 	str[5] = '\0';
-    return str;
+	return str;
 }
 
 void testme()
 {
-  int tcCount = 0;
-  char *s;
-  char c;
-  int state = 0;
-  while (1)
-  {
-    tcCount++;
-    c = inputChar();
-    s = inputString();
-    printf("Iteration %d: c = %c, s = %s, state = %d\n", tcCount, c, s, state);
-    if (c == '[' && state == 0) state = 1;
-    if (c == '(' && state == 1) state = 2;
-    if (c == '{' && state == 2) state = 3;
-    if (c == ' '&& state == 3) state = 4;
-    if (c == 'a' && state == 4) state = 5;
-    if (c == 'x' && state == 5) state = 6;
-    if (c == '}' && state == 6) state = 7;
-    if (c == ')' && state == 7) state = 8;
-    if (c == ']' && state == 8) state = 9;
-    if (s[0] == 'r' && s[1] == 'e'
-       && s[2] == 's' && s[3] == 'e'
-       && s[4] == 't' && s[5] == '\0'
-       && state == 9)
-    {
-      printf("error ");
-      exit(200);
-    }
-  }
-  free(s);
+	int tcCount = 0;
+	char *s;
+	char c;
+	int state = 0;
+	while (1)
+	{
+		tcCount++;
+		c = inputChar();
+		s = inputString();
+		printf("Iteration %d: c = %c, s = %s, state = %d\n", tcCount, c, s, state);
+		if (c == '[' && state == 0) state = 1;
+		if (c == '(' && state == 1) state = 2;
+		if (c == '{' && state == 2) state = 3;
+		if (c == ' '&& state == 3) state = 4;
+		if (c == 'a' && state == 4) state = 5;
+		if (c == 'x' && state == 5) state = 6;
+		if (c == '}' && state == 6) state = 7;
+		if (c == ')' && state == 7) state = 8;
+		if (c == ']' && state == 8) state = 9;
+		if (s[0] == 'r' && s[1] == 'e'
+			&& s[2] == 's' && s[3] == 'e'
+			&& s[4] == 't' && s[5] == '\0'
+			&& state == 9)
+		{
+			printf("error ");
+			exit(200);
+		}
+	}
+	free(s);
 }
 
 
 int main(int argc, char *argv[])
 {
-    srand(time(NULL));
-    testme();
-    return 0;
+	srand(time(NULL));
+	testme();
+	return 0;
 }
